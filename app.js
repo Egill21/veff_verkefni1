3e24b609bf4bd2e316739edc042393f6cdce30a6
Egill Ragnarsson <egill@tg-dw243.rhi.hi.is>
2019-01-24 22:50:19 +0000

    Verkefni_1-Allt

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
-/* todo, setja upp express */
+const express = require('express');
+const path = require('path');
+const lectures = require('./lectures.js');
+
+const app = express();
+const port = 3000;
+const hostname = '127.0.0.1';
+const publicPath = path.join(__dirname, 'public');
+const viewPath = path.join(__dirname, 'views');
+
+app.set('views', viewPath);
+app.set('view engine', 'ejs');
+
+app.use(express.static(publicPath));
+app.use('/', lectures);
+
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Forsíða' });
+});
+
+function notFoundHandler(req, res, next) { // eslint-disable-line
+  const type = '404 Error';
+  const title = 'Síðan fannst ekki.';
+  const message = 'Efnið sem þú ert að leita að finnst ekki.';
+  res.status(404).render('error', { title, type, message });
+}
+
+function errorHandler(err, res, req, next) { // eslint-disable-line
+  const type = '500 Error';
+  const title = 'Innri netþjóna villa';
+  const message = 'Netþjóninn klúðraði eitthverju. Sorry';
+  res.status(500).render('error', { title, type, message });
+}
+
+app.use(notFoundHandler);
+app.use(errorHandler);
+
+app.listen(port, () => {
+  console.info(`Server running at http://${hostname}:${port}/`);
+});
--- lectures.js
+++ lectures.js
@@ -1,20 +1,48 @@
 const express = require('express');
+const util = require('util');
+const fs = require('fs');
+const items = require('./items.js');
 
 const router = express.Router();
+
+const readFileAsync = util.promisify(fs.readFile);
 
 function catchErrors(fn) {
   return (req, res, next) => fn(req, res, next).catch(next);
 }
 
+async function lesaJSON() {
+  const file = await readFileAsync('./lectures.json');
+  return JSON.parse(file);
+}
+
 async function list(req, res) {
-  /* todo útfæra */
+  const title = 'Fyrirlestrar';
+  const data = await lesaJSON();
+
+  res.render('index', { title, lectures: data.lectures });
 }
 
 async function lecture(req, res, next) {
-  /* todo útfæra */
+  const { slug } = req.params;
+  const lectures = await lesaJSON();
+
+  const foundLecture = lectures.lectures.find(a => a.slug === slug);
+
+  if (!foundLecture) {
+    return next();
+  }
+
+  const { title } = foundLecture;
+  const { category } = foundLecture;
+  const html = items.createContent(foundLecture.content);
+
+  return res.render('lecture', {
+    title, html, category, lecture: foundLecture,
+  });
 }
 
-router.get('/', catchErrors(list));
 router.get('/:slug', catchErrors(lecture));
+router.get('/', catchErrors(list));
 
 module.exports = router;
